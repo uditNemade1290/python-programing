265e8c707442794f21dcb3f2746d97f3df0f54d3
uditNemade1290 <udn1290@gmail.com>
2026-02-08 11:14:13 +0530

    tuple.py

---
+++ tuple.py
@@ -0,0 +1,18 @@
+#Tuple Operations
+
+# 3.1 Create and access tuple
+my_tuple = (10, 20, 30)
+print("Tuple:", my_tuple)
+print("First element:", my_tuple[0])
+
+# 3.2 Nested tuple
+nested_tuple = (1, (2, 3), 4)
+print("Nested tuple:", nested_tuple)
+
+# 3.3 Repetition of tuple
+print("Repetition:", my_tuple * 2)
+
+# 3.4 Concatenation of tuple
+tuple_a = (1, 2)
+tuple_b = (3, 4)
+print("Concatenated tuple:", tuple_a + tuple_b)
